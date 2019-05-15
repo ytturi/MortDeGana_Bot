ba9cc787e40beaa307b234ca0d6a88bb70d7976c
Ytturi <ytturi@protonmail.com>
2019-05-16 01:37:34 +0200

    INIT COMMIT

---
+++ meldebot/meldebot.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+updater = Updater('YOUR TOKEN HERE')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
