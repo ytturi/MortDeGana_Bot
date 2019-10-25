a4a12d19268ec40974ff8fa06a1efb99cfd05187
Gerard Dalmau <gddalmau@gmail.com>
2019-10-25 18:34:44 +0200

    ADD send typing action decorator

--- meldebot/mel.py
+++ meldebot/mel.py
@@ -7,14 +7,29 @@
 # - MOTO: Send motorbike GIF
 ###############################################################################
 from telegram.ext import CommandHandler
+import telegram
 from random import randint
 from configparser import ConfigParser
 from os.path import expanduser
 from logging import getLogger
 from requests import get as http_get
+from functools import wraps
 
 # Self imports
 from meldebot.confs import get_giphy_api_key
+
+
+def send_typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(chat_id=update.effective_message.chat_id,
+                                     action=telegram.ChatAction.TYPING)
+        return func(update, context,  *args, **kwargs)
+
+    return command_func
+
 
 def get_gif_url(params):
     logger = getLogger('GIF')
