db557dfb16a8015bbe0befd7244d7c16132bac7b
ytturi <ytturi@protonmail.com>
2020-10-12 20:03:26 +0200

    Ignore mypy files in check.py

--- checks.sh
+++ checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pyfiles=$(find -type f | grep .py | grep -v 'pycache' | grep -v 'build')
+pyfiles=$(find -type f | grep .py | grep -v 'pycache' | grep -v 'build' | grep -v 'mypy')
 
 if [ "$pyfiles" ]
 then
