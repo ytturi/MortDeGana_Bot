146ff5767591fa7d5fac313528e9710532f0a11b
ytturi <ytturi@protonmail.com>
2022-01-10 18:04:03 +0100

    Replicate CI on `checks.sh`
    Add `isort` so that we fix imports
    Add `python setup.py test` so that it also runs the tests

--- checks.sh
+++ checks.sh
@@ -5,6 +5,8 @@ pyfiles=$(find . -type f | grep .py | grep -v 'pycache' | grep -v 'build' | grep
 if [ "$pyfiles" ]
 then
   black $pyfiles
+  isort $pyfiles
 fi
 
 mypy meldebot
+python setup.py test
